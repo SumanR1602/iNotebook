e722edbee9125d71a9f10dc1b333afed29f38436
SumanR1602 <120008720+SumanR1602@users.noreply.github.com>
2024-08-22 19:05:03 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,9 @@ const port = 5000
 app.use(cors())
 app.use(express.json());
 
+app.get("/",(req,res)=>{
+  res.json(express.json())
+})
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
 

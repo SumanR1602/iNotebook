1b366120cf8d86ca43e082a3f54b27527ebe3c94
SumanR1602 <120008720+SumanR1602@users.noreply.github.com>
2024-08-22 21:40:21 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,6 @@
 const mongoose=require('mongoose')
-const mongoURI = "mongodb://127.0.0.1:27017/iNotebook";
+const mongoURI = "mongodb+srv://Suman:<Suman@Atlas>@inotebookcluster.du83u.mongodb.net/?retryWrites=true&w=majority&appName=iNotebookCluster
+";
 
 const connectToMongo=()=>{
     mongoose.connect(mongoURI).then(()=>console.log("Connected")).catch((e)=>console.log(e.message))

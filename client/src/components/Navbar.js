59e392c47bc46d0bd84032881d83e209638f013b
SumanR1602 <120008720+SumanR1602@users.noreply.github.com>
2024-08-23 11:55:28 +0530

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -16,25 +16,24 @@ const Navbar = () => {
     };
 
     return (
-        <div>
-            <nav className="navbar navbar-expand-lg" style={{ borderBottom: "1.5px solid black" }}>
-                <div className="container-fluid">
-                    <div className="d-flex" style={{ margin: "0.5rem", marginLeft: "12rem" }}>
-                        <img src={logo} alt="iNotebook Logo" style={{ height: "50px" }} />
-                        <Link className={`navbar-brand ms-2 fs-3 ${!isLoggedIn ? 'disabled' : ''}`} to="." onClick={handleClick} >iNotebook</Link>
-                    </div>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="">
-                        {!localStorage.getItem('token') ? <form className="d-flex" style={{ marginRight: "6rem" }}>
-                            <Link className="mx-2 fs-6 px-4 btn-custom" to="/login">Login</Link>
-                            <Link className="mx-2 fs-6 px-4 btn-gradient" to="/signup" >SignUp</Link>
-                        </form> : <button className='btn btn-primary mx-2 fs-6 px-4 btn-gradient' onClick={handleLogout} >Logout</button>}
-                    </div>
+        <nav className="navbar navbar-expand-lg" style={{ borderBottom: "1.5px solid black" }}>
+            <div className="container-fluid">
+                <div className="d-flex justify-content-center align-items-center mx-auto">
+                    <img src={logo} alt="iNotebook Logo" style={{ height: "50px" }} />
+                    <Link className={`navbar-brand ms-2 fs-3 ${!isLoggedIn ? 'disabled' : ''}`} to="." onClick={handleClick}>iNotebook</Link>
                 </div>
-            </nav>
-        </div>
+                <div className="d-flex justify-content-center align-items-center mx-auto mt-3 mt-lg-0">
+                    {!localStorage.getItem('token') ? (
+                        <form className="d-flex">
+                            <Link className="mx-2 fs-6 px-4 btn-custom" to="/login">Login</Link>
+                            <Link className="mx-2 fs-6 px-4 btn-gradient" to="/signup">SignUp</Link>
+                        </form>
+                    ) : (
+                        <button className='btn btn-primary mx-2 fs-6 px-4 btn-gradient' onClick={handleLogout}>Logout</button>
+                    )}
+                </div>
+            </div>
+        </nav>
     )
 }
 

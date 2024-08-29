bfdd11c4355cc9230e698eb826bac1aa6db3f54f
SumanR1602 <120008720+SumanR1602@users.noreply.github.com>
2024-08-29 22:56:51 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const app = express()
 const port = 5000
 
 app.use(cors({
-  origin: "https://i-notebook-site.vercel.app",
+  origin: "https://inotebook-site.vercel.app/",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization","auth-token"],
   optionsSuccessStatus: 200
